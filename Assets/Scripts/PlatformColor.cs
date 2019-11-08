53113aa3a8c8e6ff626c1c8f92bb5430cf7a5ba1
DunchaLine <47224391+DunchaLine@users.noreply.github.com>
2019-11-08 16:06:48 +0300

    change-color-platforms
    Add changing color by the touch from the ball

---
+++ Assets/Scripts/PlatformColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformColor : MonoBehaviour
+{
+    SpriteRenderer _renderer;
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _renderer.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
