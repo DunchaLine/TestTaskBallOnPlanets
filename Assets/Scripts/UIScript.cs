e48a2731dabb60b8ed95e2c6a5a6d630a3e0a8da
DunchaLine <47224391+DunchaLine@users.noreply.github.com>
2019-11-08 14:41:04 +0300

    menu-scene
    Add menu scene with 3 buttons to each planets

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    string _planetName;
+    public void EarthButton()
+    {
+        PlayerPrefs.DeleteAll();
+        _planetName = "Earth";
+        PlayerPrefs.SetString("Planet", _planetName);
+        SceneManager.LoadScene("main");
+    }
+
+    public void MoonButton()
+    {
+        PlayerPrefs.DeleteAll();
+        _planetName = "Moon";
+        PlayerPrefs.SetString("Planet", _planetName);
+        SceneManager.LoadScene("main");
+    }
+
+    public void JupiterButton()
+    {
+        PlayerPrefs.DeleteAll();
+        _planetName = "Moon";
+        PlayerPrefs.SetString("Planet", _planetName);
+        SceneManager.LoadScene("main");
+    }
+}
