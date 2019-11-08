3b8072e37130551c9f0f3871e6327b507d4669dc
DunchaLine <47224391+DunchaLine@users.noreply.github.com>
2019-11-08 15:56:39 +0300

    changing-color-planet
    Changing color depends on planet

---
+++ Assets/Scripts/BackgroundColor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColor : MonoBehaviour
+{
+    public Color _earthColor, _moonColor, _jupiterColor;
+    string _planetName;
+    Camera cam;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _planetName = PlayerPrefs.GetString("Planet");
+        if (_planetName == "Earth")
+        {
+            EarthColor();
+        }
+        if (_planetName == "Moon")
+        {
+            MoonColor();
+        }
+        if (_planetName == "Jupiter")
+        {
+            JupiterColor();
+        }
+    }
+    void EarthColor()
+    {
+        cam.backgroundColor = _earthColor;
+    }
+    void MoonColor()
+    {
+        cam.backgroundColor = _moonColor;
+    }
+    void JupiterColor()
+    {
+        cam.backgroundColor = _jupiterColor;
+    }
+}
